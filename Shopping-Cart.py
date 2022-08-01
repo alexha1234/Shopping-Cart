ffa8fae073d35bb077262709a6171326c3bed35e
alexha1234 <76671695+alexha1234@users.noreply.github.com>
2022-07-31 21:52:13 -0400

    Update Shopping-Cart.py

--- Shopping-Cart.py
+++ Shopping-Cart.py
@@ -37,7 +37,7 @@ def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
 selected_products = [] 
-
+total_price = 0
 while True:
     selected_id = input("Please input a product id, or 'DONE': " ) #will be a string
 
@@ -48,6 +48,7 @@ while True:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0] # this will trigger an IndexError if there are no matching products
         selected_products.append(matching_product)
+        total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT(S):" + matching_product["name"] + " " + str(matching_product["price"]))
 
-
+print("TOTAL PRICE:" + str(to_usd(total_price))
